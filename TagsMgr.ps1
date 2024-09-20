27b874756afbc2b869225c1d0aa0bd1781e08175
Weslley Murdock <weslleyluiz@outlook.com.br>
2024-09-20 10:41:01 -0300

    update helper script

--- TagsMgr.ps1
+++ TagsMgr.ps1
@@ -3,31 +3,56 @@
 #>
 
 param(
-	[string] $version,
-	[bool] $deleteRemote = False
+	[switch]	$help,
+	[switch]	$regenerate,
+	[switch]    $nocreate,
+	[string]	$version
 )
 
-if ($args[0] -Eq "--help")
+if ($help)
 {
 	Write-Host "Tags Manager pwsh script. It helps (re)generate a tag and publish to remote git repo. It also delete remote tag and local tag in case of CI/CD publish error"
 	Write-Host "Usage:"
 	Write-Host "      ./TagsMgr.ps1 -version 2.0.212"
 	Write-Host "	      - It Creates a tag and push to remote"
-	Write-Host "      ./TagsMgr.ps1 -version 2.0.212 -deleteRemote False"
+	Write-Host "      ./TagsMgr.ps1 -version 2.0.212 -regenerate"
 	Write-Host "           - It delete remote and local tags to create and push new tags"
-	Write-Host "      ./TagsMgr.ps1 --help"
+	Write-Host "      ./TagsMgr.ps1 -regenerate -nocreate"
+	Write-Host "           - It delete remote and local tags only"
+	Write-Host "      ./TagsMgr.ps1 -help"
 	Write-Host "           - It shows this output"
 	Write-Host "Version: 1.0"
 	exit 0;
 }
 
+$tag = "v" + $version
+$pattern = '^[v][\d+].[\d+].[\d+].*[\w*]*$'
 
-if ($deleteRemote -Eq True){
-	git push --delete origin "v$(version)"
-	git tag -d "v$(version)"
+
+if ($tag -match $pattern)
+{
+	Write-Host "Valid tag name created"
+}
+else
+{
+	Write-Error "tag name not contains valid version. Exiting"
+	exit 1
 }
 
-git tag "v$(version)"
-git push origin --Tags
+if ($regemerate)
+{
+	git push --delete origin $tag
+	git tag -d $tag	
+}
+
+if ($regemerate -and $nocreate)
+{
+	Write-Host "Done"
+	exit 0;
+}
+
+git tag $tag
+git push origin --tags
+
 
 Write-Host "Done."
